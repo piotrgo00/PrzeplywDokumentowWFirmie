4ef2222facbe73bc0808f30cd4625f09859017ea
piotrgo00 <65816799+piotrgo00@users.noreply.github.com>
2022-01-21 16:07:26 +0100

    some fixes so the project works :)

--- Models/Commodity.cs
+++ Models/Commodity.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Types.Sql;
+﻿//using Microsoft.Analytics.Interfaces;
+//using Microsoft.Analytics.Types.Sql;
+using PrzeplywDokumentowWFirmie.Logic;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,7 +14,7 @@ namespace PrzeplywDokumentowWFirmie.Models
         IItem commodity;
         int quantity;
 
-        public IItem Commodity { get => commodity; set => commodity = value; }
+        public IItem Item { get => commodity; set => commodity = value; }
         public int Quantity { get => quantity; set => quantity = value; }
     }
 }
