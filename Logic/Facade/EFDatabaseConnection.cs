61268a7a2fbc4924c270d0e5226321f230986918
piotrgo00 <65816799+piotrgo00@users.noreply.github.com>
2022-01-22 17:53:11 +0100

    not so shit error handling

--- Logic/Facade/EFDatabaseConnection.cs
+++ Logic/Facade/EFDatabaseConnection.cs
@@ -2,8 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace PrzeplywDokumentowWFirmie.Logic.Facade
 {
@@ -14,17 +17,17 @@ namespace PrzeplywDokumentowWFirmie.Logic.Facade
         public void addElectronicItem(IItem item)
         {
             db.ElectronicItems.Add((ElectronicItem)item);
-            db.SaveChanges();
+            this.SaveChanges();
         }
         public void addConsumableItem(IItem item)
         {
             db.ConsumableItems.Add((ConsumableItem)item);
-            db.SaveChanges();
+            this.SaveChanges();
         }
         public void addFurnitureItem(IItem item)
         {
             db.FurnitureItems.Add((FurnitureItem)item);
-            db.SaveChanges();
+            this.SaveChanges();
         }
 
         public void addWarehouse()
@@ -61,30 +64,52 @@ namespace PrzeplywDokumentowWFirmie.Logic.Facade
             return db.FurnitureItems.ToList();
         }
 
+        [HandleError(View = "Error")]
         public void SaveChanges()
         {
-            db.SaveChanges();
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                var sqlException = ex.GetBaseException() as SqlException;
+
+                if (sqlException != null)
+                {
+                    var number = sqlException.Number;
+
+                    if (number == 547)
+                    {
+                        throw new Exception("Must delete Commodities before Items");
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+            }
         }
 
         public void deleteElectronicItem(int id)
         {
             ElectronicItem item = db.ElectronicItems.Find(id);
             db.ElectronicItems.Remove(item);
-            db.SaveChanges();
+            this.SaveChanges();
         }
 
         public void deleteConsumableItem(int id)
         {
             ConsumableItem item = db.ConsumableItems.Find(id);
             db.ConsumableItems.Remove(item);
-            db.SaveChanges();
+            this.SaveChanges();
         }
 
         public void deleteFurnitureItem(int id)
         {
             FurnitureItem item = db.FurnitureItems.Find(id);
             db.FurnitureItems.Remove(item);
-            db.SaveChanges();
+            this.SaveChanges();
         }
 
         public void dispose()
@@ -95,19 +120,19 @@ namespace PrzeplywDokumentowWFirmie.Logic.Facade
         public void editElectronicItem(ElectronicItem item)
         {
             db.Entry(item).State = EntityState.Modified;
-            db.SaveChanges();
+            this.SaveChanges();
         }
 
         public void editConsumableItem(ConsumableItem item)
         {
             db.Entry(item).State = EntityState.Modified;
-            db.SaveChanges();
+            this.SaveChanges();
         }
 
         public void editFurnitureItem(FurnitureItem item)
         {
             db.Entry(item).State = EntityState.Modified;
-            db.SaveChanges();
+            this.SaveChanges();
         }
     }
 }
