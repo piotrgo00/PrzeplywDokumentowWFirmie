ba93ba6e72ecebbbe642563b7cf505594e8fd92a
Łukasz <chojecki2000@gmail.com>
2022-01-22 13:49:10 +0100

    tryin to make order crud, little fix with state pattern

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using PrzeplywDokumentowWFirmie.Logic.State;
@@ -10,18 +11,25 @@ namespace PrzeplywDokumentowWFirmie.Models
     public class Order
     {
         public int OrderId { get; set; }
-        public Firm Buyer { get; set; }
-        public virtual ICollection<Commodity> Commodities { get; set; }
-        public Invoice Invoice { get; set; }
+        public int BuyerId { get; set; }
+        public int InvoiceId { get; set; }
 
+        public virtual Firm Buyer { get; set; }
+        public virtual Invoice Invoice { get; set; }
+        public virtual ICollection<Commodity> Commodities { get; set; }
+        
+        [NotMapped]
         private State _state = null;
 
+        public Order(State state)
+        {
+            this.TransitionTo(state);
+        }
         public void TransitionTo(State state)
         {
             this._state = state;
             this._state.SetOrder(this);
         }
-
         public Invoice GetInvoice()
         {
             return this._state.GetInvoice();
