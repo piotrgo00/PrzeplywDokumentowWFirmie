4c6752ad286d1d15a64837876a87ea33b3989867
Konrad Wasilewski <konradwasilewski@vp.pl>
2022-01-24 19:54:56 +0100

    Abstract factory early solution

--- Logic/AbstractFactory/InvoiceAbstractFactory.cs
+++ Logic/AbstractFactory/InvoiceAbstractFactory.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrzeplywDokumentowWFirmie.Models;
 
 namespace PrzeplywDokumentowWFirmie.Logic.AbstractFactory
 {
     public interface InvoiceAbstractFactory
     {
-        IInvoice GetCreateInvoice();
+        Invoice GetInvoice();
+        String getHTML(Order order);
     }
 }
